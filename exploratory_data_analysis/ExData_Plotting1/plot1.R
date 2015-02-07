f6dc0cb5dc13d95b51c582d37af7229300c566b1
John Bobo <treybobo@me.com>
2015-02-07 15:07:57 -0500

    Reclone/fork from rdpeng

---
+++ exploratory_data_analysis/ExData_Plotting1/plot1.R
@@ -0,0 +1,12 @@
+file <- c("../household_power_consumption.txt")
+power <- read.csv(file, header=TRUE,sep=";",na.strings='?')
+names(power)
+head(power)
+str(power$Date)
+power_subset <- power[(power$Date == "1/2/2007") | (power$Date == "2/2/2007"),]
+
+hist(power_subset$Global_active_power, col = "red", breaks = 12,
+     xlab = "Global Active Power(kilowatts)", main = "Global Active Power")
+
+dev.copy(png,file = "plot1.png", height = 480, width = 480, bg = "transparent")
+dev.off()
