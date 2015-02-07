e1837737828eae78d3646b5536e4e455c6acd832
John Bobo <treybobo@me.com>
2015-02-07 16:26:10 -0500

    Change backgrounds

--- exploratory_data_analysis/ExData_Plotting1/plot1.R
+++ exploratory_data_analysis/ExData_Plotting1/plot1.R
@@ -5,5 +5,5 @@ power_subset <- power[(power$Date == "1/2/2007") | (power$Date == "2/2/2007"),]
 hist(power_subset$Global_active_power, col = "red", breaks = 12,
      xlab = "Global Active Power(kilowatts)", main = "Global Active Power")
 
-dev.copy(png,file = "plot1.png", height = 480, width = 480, bg = "transparent")
+dev.copy(png,file = "plot1.png", height = 480, width = 480, bg = "white")
 dev.off()
