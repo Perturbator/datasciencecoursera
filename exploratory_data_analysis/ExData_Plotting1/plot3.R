8a7e33619d35f4fb8c90943396d40a762ed0cb16
John Bobo <treybobo@me.com>
2015-02-07 18:39:15 -0500

    Finish transparent backgrounds

--- exploratory_data_analysis/ExData_Plotting1/plot3.R
+++ exploratory_data_analysis/ExData_Plotting1/plot3.R
@@ -2,7 +2,7 @@ file <- c("household_power_consumption.txt")
 power <- read.csv(file, header=TRUE,sep=";",na.strings='?')
 power <- power[(power$Date == "1/2/2007") | (power$Date == "2/2/2007"),]
 power$datetime<-as.POSIXct(paste(power$Date,power$Time), format="%d/%m/%Y %H:%M:%S")
-plot(power$datetime,power$Sub_metering_1,type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(power$datetime,power$Sub_metering_1,type="l", xlab="", ylab="Energy sub meeting")
 lines(power$datetime,power$Sub_metering_2, col = "red")
 lines(power$datetime,power$Sub_metering_3, col = "blue")
 legend("topright", 
@@ -14,5 +14,5 @@ legend("topright",
        cex = .75
 )
 
-dev.copy(png, file = "plot3.png", height = 480, width = 480, bg = "white")
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
 dev.off()
