01f71ee9c3eba05259ae7760357cba4d67538f34
John Bobo <treybobo@me.com>
2015-02-07 16:35:25 -0500

    make backgrounds white on plots

--- exploratory_data_analysis/ExData_Plotting1/plot3.R
+++ exploratory_data_analysis/ExData_Plotting1/plot3.R
@@ -14,5 +14,5 @@ legend("topright",
        cex = .75
 )
 
-dev.copy(png,file = "plot3.png", height = 480, width = 480, bg = "white")
+dev.copy(png, file = "plot3.png", height = 480, width = 480, bg = "white")
 dev.off()
