c31c64ca944b9b634aedc863775dc22b3034ff47
John Bobo <treybobo@me.com>
2015-02-07 16:22:39 -0500

    Add plot and code for plot 3

---
+++ exploratory_data_analysis/ExData_Plotting1/plot3.R
@@ -0,0 +1,18 @@
+file <- c("household_power_consumption.txt")
+power <- read.csv(file, header=TRUE,sep=";",na.strings='?')
+power <- power[(power$Date == "1/2/2007") | (power$Date == "2/2/2007"),]
+power$datetime<-as.POSIXct(paste(power$Date,power$Time), format="%d/%m/%Y %H:%M:%S")
+plot(power$datetime,power$Sub_metering_1,type="l", xlab="", ylab="Global Active Power (kilowatts)")
+lines(power$datetime,power$Sub_metering_2, col = "red")
+lines(power$datetime,power$Sub_metering_3, col = "blue")
+legend("topright", 
+       legend = c("Sub_metering_1", 
+                  "Sub_metering_2", 
+                  "Sub_metering_3"), 
+       col = c("black", "red", "blue"), 
+       lwd = .75, 
+       cex = .75
+)
+
+dev.copy(png,file = "plot3.png", height = 480, width = 480, bg = "transparent")
+dev.off()
