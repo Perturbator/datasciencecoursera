7a8932d3170dc2df0daab1337f0723f6710604bb
John Bobo <treybobo@me.com>
2015-02-07 16:05:07 -0500

    Add r code for plot2

---
+++ exploratory_data_analysis/ExData_Plotting1/plot2.R
@@ -0,0 +1,7 @@
+file <- c("household_power_consumption.txt")
+power <- read.csv(file, header=TRUE,sep=";",na.strings='?')
+power <- power[(power$Date == "1/2/2007") | (power$Date == "2/2/2007"),]
+power$datetime<-as.POSIXct(paste(power$Date,power$Time), format="%d/%m/%Y %H:%M:%S")
+plot(power$datetime,power$Global_active_power,type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.copy(png,file = "plot2.png", height = 480, width = 480, bg = "transparent")
+dev.off()
