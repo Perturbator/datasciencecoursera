f58cc026d68f61bae43a633656b024ed20021e21
John Bobo <treybobo@me.com>
2015-02-07 18:37:46 -0500

    Change plots back to transparent

--- exploratory_data_analysis/ExData_Plotting1/plot1.R
+++ exploratory_data_analysis/ExData_Plotting1/plot1.R
@@ -1,8 +1,7 @@
 file <- c("household_power_consumption.txt")
 power <- read.csv(file, header=TRUE,sep=";",na.strings='?')
-power_subset <- power[(power$Date == "1/2/2007") | (power$Date == "2/2/2007"),]
-
-hist(power_subset$Global_active_power, col = "red", breaks = 12,
+power <- power[(power$Date == "1/2/2007") | (power$Date == "2/2/2007"),]
+hist(power$Global_active_power, col = "red", breaks = 12,
      xlab = "Global Active Power(kilowatts)", main = "Global Active Power")
 
 dev.copy(png,file = "plot1.png", height = 480, width = 480, bg = "white")
